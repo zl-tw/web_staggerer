ac116df886425e03695992fb6f692c79965a63fb
zl <915064436@qq.com>
2020-07-31 23:29:57 +0800

    完成首页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,19 @@
 // 每次调用 ajax 的时候都会先调用 ajaxPrefilter 函数
 // 在这个函数中我们可以拿到给 ajax 提供的配置对象
 $.ajaxPrefilter(function(options) {
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂在complete函数
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            location.href = '/login.html'
+            localStorage.removeItem('token')
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+$(function() {
+    var layer = layui.layer
+
+    userInfo();
+
+    function userInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            // headers: {
+            //     Authorization: localStorage.getItem('token') || ''
+            // },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                avatar(res.data)
+            },
+            // ajax的回调函数，已在 baseAPI 全局挂载
+            // complete: function(res) {
+            //     console.log(res.responseJSON.message);
+            //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //         location.href = '/login.html'
+            //         localStorage.removeItem('token')
+            //     }
+            // }
+        })
+    }
+
+    //渲染图片头像
+    function avatar(data) {
+        var name = data.nickname || data.username;
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+        if (data.user_pic) {
+            $('.layui-nav-img').attr('src', data.user_pic).show();
+            $('.text-avatar').hide();
+        } else {
+            $('.layui-nav-img').hide();
+            var first = name[0].toUpperCase();
+            $('.text-avatar').html(first).show()
+        }
+    }
+
+    // 退出功能
+    $('#exit').on('click', function() {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            location.href = '/login.html';
+            localStorage.removeItem('token');
+
+            layer.close(index);
+        });
+
+    })
+})
